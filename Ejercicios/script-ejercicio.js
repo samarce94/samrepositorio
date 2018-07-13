e2447dffaea289e3d90e21fb32b1ccec5ec7e942
Victor Quiñonez <victor.quinonez.paez@accenture.com>
2018-07-12 20:32:15 -0500

    ejercicio1

---
+++ Ejercicios/script-ejercicio.js
@@ -0,0 +1,36 @@
+/* 
+    Ejercicio: Cada que se clique el cuadro: 
+        - Switchear el color entre rojo y azul
+        - Agregar dentro del cuadro el nombre del color anterior.
+*/
+
+// 1. Separar el elemento en una variable
+var squareEjercicio = document.getElementById('square-test');
+
+// 2. Asignar atributo
+squareEjercicio.setAttribute('data-color', 'red');
+
+// 3. Agregar Event Listener
+squareEjercicio.addEventListener('click', function(evento) {
+    // 4. Obtener elemento cliqueado
+    var elementoSeleccionado = evento.target;
+    // 5. Obtener color guardado
+    var color = elementoSeleccionado.getAttribute('data-color');
+    
+    // 6. Asignar el color de fondo
+    elementoSeleccionado.style.background = color;
+    
+    // 7. validamos el color
+    if (color === 'red') {
+        elementoSeleccionado.setAttribute('data-color', 'blue');
+    } else {
+        elementoSeleccionado.setAttribute('data-color', 'red');
+    }
+    
+    // 8. Crear un elemento virtual
+    var nuevoTexto = document.createElement('span');
+    // 9. Agregar el nombre del color
+    nuevoTexto.innerHTML = color;
+    // 10. Agregar el elemento
+    elementoSeleccionado.appendChild(nuevoTexto);
+});
