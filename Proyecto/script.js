13f65d84a70c100b121e529954e5532c9dfd2208
Victor Quiñonez <victor.quinonez.paez@accenture.com>
2018-07-14 15:36:13 -0500

    Ejercicio de recuerdo

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -8,6 +8,7 @@
 var boton = document.getElementById('agregar');
 var texto = document.getElementById('inputTxt');
 var lista = document.getElementById('list');
+var botonBorrar = document.getElementById('borrar');
 
 // 2. Agregar listener
 boton.addEventListener('click', function(){
@@ -24,7 +25,6 @@ boton.addEventListener('click', function(){
 });
 
 texto.addEventListener('keyup', function(evento){
-    console.log(evento);
     if (evento.key === 'Enter') {
         // 3. Validar si existe
         if ( texto.value !== '' ) {
@@ -36,5 +36,17 @@ texto.addEventListener('keyup', function(evento){
             // 5. Limpiar texto
             texto.value = '';
         }
+    }
+});
+
+// Agregar eventListener para el boton de borrar
+botonBorrar.addEventListener('click', function() {
+    // Validar si existen elementos en la lista
+    if (lista.childNodes.length !== 0) {
+        // Limpiar lista
+        lista.innerText = '';
+    } else {
+        // Si no hay elementos, desplegar una alerta
+        alert('No hay elementos que borrar');
     }
 });
