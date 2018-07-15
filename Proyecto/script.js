16bb6f1764f78914f6034eee15b3f4914363335e
Victor Quiñonez <victor.quinonez.paez@accenture.com>
2018-07-15 18:18:51 -0500

    ejercicio7

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -14,9 +14,7 @@ boton.addEventListener('click', function(){
     // 3. Validar si existe
     if ( texto.value !== '' ) {
         // 4. Crear elemento, llenarlo y agregarlo
-        var item = document.createElement('li');
-        item.innerHTML = texto.value;
-        lista.appendChild(item);
+        addElement(texto.value);
 
         // 5. Limpiar texto
         texto.value = '';
@@ -28,12 +26,54 @@ texto.addEventListener('keyup', function(evento){
         // 3. Validar si existe
         if ( texto.value !== '' ) {
             // 4. Crear elemento, llenarlo y agregarlo
-            var item = document.createElement('li');
-            item.innerHTML = texto.value;
-            lista.appendChild(item);
+            addElement(texto.value);
     
             // 5. Limpiar texto
             texto.value = '';
         }
     }
+});
+
+/* EJERCICIO: 
+    - Toggle clase done al li cuando se le da click
+    - remover todos los li que tienen la clase done cuando se le da click al boton limpiar
+*/
+
+// 1. Agrega elemento a lista
+function addElement(texto) {
+    var item = document.createElement('li');
+    item.innerHTML = texto;
+    // 2. Agrega event Litener al crearlo
+    item.addEventListener('click', liListener);
+    lista.appendChild(item);
+}
+
+// 3. lo que hace el listener
+function liListener (evento) {
+    // 4. Obtiene las clases
+    const classList = evento.target.classList;
+    // 5. Si contiene done
+    if (classList.contains('done')) {
+        // 6. quita la clase
+        classList.remove('done');
+    } else {
+        // 7. agrega la clase
+        classList.add('done');
+    }
+}
+
+// 8. Obtiene boton de borrar
+const removeBtn = document.getElementById('borrar');
+
+// 9. EventListener al boton de borrar
+removeBtn.addEventListener('click', function() {
+    // 10. convierte el HTML collection en array
+    const items = Array.from(lista.getElementsByClassName('done'));
+
+    // 11. Por cada item que existe 
+    items.forEach(function(item) {
+        // 12. Remueve el item
+        lista.removeChild(item);
+    });
+
 });
